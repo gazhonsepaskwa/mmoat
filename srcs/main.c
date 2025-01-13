bab945e6a703f50a91634241033ba373ce08a45b
gazhonsepaskwa <gaz.honsepaskwa@gmail.com>
2025-01-13 16:53:28 +0100

    imporved powerline

--- srcs/main.c
+++ srcs/main.c
@@ -15,7 +15,9 @@ char	*powerline(void)
 	}
 	else
 		tilt = " ";
-	printf("\n%s  MMOAT? %s%s%s%s%s %s%s%s ",
+	printf("%s----------------------------------------------\
+----------------------------------%s", POW5, RESET);
+	printf("\n%s  MMOAT %s%s%s%s%s %s%s%s ",
 		POW1, POW2, POW3, POW4, tilt, pwd, RESET, POW5, RESET);
 	return (readline(""));
 }
